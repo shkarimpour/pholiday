077daebbb1ac905b966ecbecc68b289f4c76c2cd
shk <shaahin.karimpour902@gmail.com>
2017-03-28 18:04:17 +0430

    bug fix

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 const pholiday = require('./index')
-const date = pholiday('2013-8-25 16:40:00', 'YYYY-M-D HH:mm:ss').endOf('jMonth')
+const date = pholiday().add(27, 'day')
 const momentHijri = require('moment-hijri')
 console.log(date.isHoliday())
 console.log(date.events())
